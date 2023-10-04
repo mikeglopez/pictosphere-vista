412b98b62b19db0e21b9d321b74c331bfd26017d
Mike <mikeglopez.91@gmail.com>
2023-10-04 17:24:59 -0500

    (fix) Fixes for the Slideshow
    - Only display slideshow if there are captured images
    - Only display images captured within the past 18 hours

--- server/helpers/get_images.js
+++ server/helpers/get_images.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
+const imageDateFilter = require('./image_date_filter');
 
 const getImages = basePath => {
   const files = fs.readdirSync(`${__dirname}/../../${basePath}`);
   const imageFiles = files.filter(file => file.includes('.png'));
+
+  const filteredImages = imageFiles.filter(file => imageDateFilter(file))
+
   const newBasePath = basePath.replace('public', '')
 
-  return imageFiles.map(file => `${newBasePath}/${file}`);
+  return filteredImages.map(file => `${newBasePath}/${file}`);
 };
 
 module.exports = getImages;
