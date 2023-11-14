bb56968f45f519aa9675d17da7e3e03e9c5b5965
Mike G. Lopez <mikeglopez@me.com>
2023-11-14 02:07:34 -0600

    (feat) Add Photo Album
    -Add basic photo album with all photos taken
    -Add font for VISTA text

--- src/views/PhotoAlbumView.js
+++ src/views/PhotoAlbumView.js
@@ -1,6 +1,29 @@
+import { useState, useCallback, useEffect } from 'react';
+import axios from 'axios';
+
 const PhotoAlbumView = () => {
+  const [images, setImages] = useState([]);
+
+  const getImages = useCallback(async () => {
+    const response = await axios.get('/api/images');
+    setImages([...response.data]);
+  }, []);
+
+  useEffect(() => {
+    getImages();
+  }, [getImages]);
+
   return (
-    <div className='photo-album'>This is the photo album</div>
+    <div className='photo-album'>
+      <div className='photo-album-header'>
+        <p className='logo-pictosphere'>PictoSphere</p>
+        <p className='logo-vista'>V I S T A</p>
+      </div>
+      <div className='photo-album-gallery'>
+        {/* This is the photo album */}
+        {images.map(img => (<a key={img} href={`http://10.0.0.88:3000${img}`}><img className='gallery-photo' src={`http://10.0.0.88:3000${img}`} alt={img}/></a>))}
+      </div>
+    </div>
   )
 }
 
