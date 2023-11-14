011046c8b57a4c2fdd6abdae9ab94fe93adabe43
Mike G. Lopez <mikeglopez@me.com>
2023-11-14 03:54:37 -0600

    (feat) Add Toggle to View Original/Processed Imgs
    -Add toggle slider for original/processed gallery
    -Corrected the QR Code
    -Hide uncaught errors iframe for cleaner mobile experience

--- src/views/PhotoAlbumView.js
+++ src/views/PhotoAlbumView.js
@@ -1,13 +1,23 @@
 import { useState, useCallback, useEffect } from 'react';
 import axios from 'axios';
+import { ToggleSlider }  from "react-toggle-slider";
 
 const PhotoAlbumView = () => {
   const [images, setImages] = useState([]);
+  const [isEnhanced, setIsEnhanced] = useState(false);
 
   const getImages = useCallback(async () => {
-    const response = await axios.get('/api/images');
+    const response = await axios.get('/api/images', {
+      params: {
+        process: isEnhanced ? 'processed' : 'original',
+      },
+    });
     setImages([...response.data]);
-  }, []);
+  }, [isEnhanced]);
+
+  const toggleEnhanced = () => {
+    setIsEnhanced(prevState => !prevState);
+  }
 
   useEffect(() => {
     getImages();
@@ -20,7 +30,9 @@ const PhotoAlbumView = () => {
         <p className='logo-vista'>V I S T A</p>
       </div>
       <div className='photo-album-gallery'>
-        {/* This is the photo album */}
+        <div className='enhance-toggle'>
+          <ToggleSlider onToggle={toggleEnhanced} />
+        </div>
         {images.map(img => (<a key={img} href={`http://10.0.0.88:3000${img}`}><img className='gallery-photo' src={`http://10.0.0.88:3000${img}`} alt={img}/></a>))}
       </div>
     </div>
