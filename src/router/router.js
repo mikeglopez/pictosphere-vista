4c521796dbbe00d78ba81c754b12fe6d3c9a6db2
Mike <mikeglopez.91@gmail.com>
2023-11-20 16:23:08 -0600

    (fix) Switch BrowserRouter to HashRouter
    -Switch to HashRouter for AWS S3 compatibility

--- src/router/router.js
+++ src/router/router.js
@@ -1,20 +1,17 @@
-import { createBrowserRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import App from "../App";
 import PhotoAlbumView from "../views/PhotoAlbumView";
 import ErrorView from "../views/ErrorView";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorView />
-  },
-  {
-    path: "/album",
-    element: <PhotoAlbumView />
-  }
-]);
+const router = (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />} caseSensitive />
+      <Route path="/album" element={<PhotoAlbumView />} caseSensitive />
+      <Route path="*" element={<ErrorView />} caseSensitive />
+    </Routes>
+  </HashRouter>
+);
 
 export default router;
