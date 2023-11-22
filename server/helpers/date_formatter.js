aeed334ce876b62ce4c6a1109e3a511b291dec78
Mike <mikeglopez.91@gmail.com>
2023-11-22 01:18:23 -0600

    (fix) Fix Date_Formatter
    -Fix date formatter so time and date matches current

--- server/helpers/date_formatter.js
+++ server/helpers/date_formatter.js
@@ -1,13 +1,15 @@
 const getCurrentDateTime = () => {
   const now = new Date();
-  const year = now.getFullYear().toString().slice(-2); // Get last two digits of the year (23 for 2023)
-  const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Add 1 to month since it's 0-based, and pad with '0' if needed
-  const day = now.getDate().toString().padStart(2, '0'); // Pad day with '0' if needed
-  const hours = now.getHours().toString().padStart(2, '0'); // Pad hours with '0' if needed
-  const minutes = now.getMinutes().toString().padStart(2, '0'); // Pad minutes with '0' if needed
-  const seconds = now.getSeconds().toString().padStart(2, '0'); // Pad seconds with '0' if needed
+  const year = now.getFullYear().toString().slice(-2);
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const day = now.getDate().toString().padStart(2, '0');
+  const hours = now.getHours() % 12 === 0 ? '00' : now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
 
-  return `${month}-${day}-${year}_${hours}${minutes}-${seconds}`; // Output: '09-22-23_1702-56'
+  const formattedDate = `${month}-${day}-${year}_${hours}${minutes}-${seconds}`;
+
+  return formattedDate;
 };
 
 module.exports = getCurrentDateTime;
